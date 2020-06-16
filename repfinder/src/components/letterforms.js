8112537859d658b5546b8402dcd89dc9def5f7da
fishmanjohn <john@emmaandjohnbyvan.com>
2020-06-16 15:27:07 -0700

    added headers

--- repfinder/src/components/letterforms.js
+++ repfinder/src/components/letterforms.js
@@ -2,6 +2,10 @@ import React from React
 
 function Letterforms () {
     return(
-        <div></div>
+        <div>
+            <h1>Find form letters to elected officials on various topics here.</h1>
+            <h2>Black Lives Matter</h2>
+            <h2>Protecting Public Lands</h2>
+        </div>
     )
 }
