c9698224da4a97052268d67f757e1c9958782759
fishmanjohn <john@emmaandjohnbyvan.com>
2020-06-19 11:18:13 -0700

    added functonality to ind form

--- repfinder/src/components/findRep.js
+++ repfinder/src/components/findRep.js
@@ -1,11 +1,31 @@
-import React from React 
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+ 
 
 function FindRep () {
+    const [form, setform] = useState({
+        adress:''
+    });
+
+    const handleChanges = e => {
+        setform({
+          [e.target.name]: e.target.value
+        });
+      };
+    const handleSubmit = e =>{
+        e.preventDefault();
+
+    }
 
     return(
         <div>
             <form onSubmit = {handleSubmit} className = 'repForm'> 
-                <input placeholder = 'Adress' type = 'text' />
+                <input 
+                name = 'adress'
+                placeholder = 'Adress'
+                onChange = {handleChanges}
+                 />
             </form>
         </div>
     )
