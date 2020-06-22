e7ba84bb972b822c56dea8360a846622c6a21d31
fishmanjohn <john@emmaandjohnbyvan.com>
2020-06-22 14:47:07 -0700

    added desc to about

--- repfinder/src/components/about.js
+++ repfinder/src/components/about.js
@@ -2,7 +2,15 @@ import React from React
 
 function About () {
     return(
-        <div></div>
+        <div>
+            <h1>About</h1>
+            <h3>An online recouce for remote activisim.</h3>
+            <p>Repfinder aims to help activists conect to their local and federal representitves, as well as letter templates and reading material about issues that face us today.
+            During this time of political upheavil and an unprecidented world wide pandemic it is important that people maintain social distancing but also confront the important issues head on.
+            Repfinder is designed to be used anonomusly to protect the activists it serves no user data or metrics will be taken from this site by its creator.</p>
+            
+
+        </div>
     )
 }
 export default About
